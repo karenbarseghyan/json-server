0bc889587aec48e184ec6368ac7e0e7f2acc569e
karenbarseghyan <karenbarseghyan90@gmail.com>
2021-01-28 02:25:05 +0400

    JSON Data Fetching

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const container = document.querySelector('.blogs');
+
+const renderPosts = async () => {
+    let uri = 'http://localhost:3000/posts';
+
+    const res = await fetch(uri);
+    const posts = await res.json();
+    // console.log(posts);
+    
+    let template ="";
+    posts.forEach(post => {
+        template += `
+            <div class = "post">
+                <h2>
+                    ${post.title}
+                </h2>
+                <p>
+                    ${post.likes}
+                </p>
+                <a href = "/details.html">read more </a>
+            </div>
+        `        
+    })
+container.innerHTML = template;
+}
+window.addEventListener('DOMContentLoaded', ()=>renderPosts());
